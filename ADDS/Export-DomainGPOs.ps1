37d36ea90079e5858a8b2b6d6ca9586177acbf73
Luis Sousa <67331339+luisfasousa@users.noreply.github.com>
2025-05-21 17:05:23 +0100

    Create Export-DomainGPOs.ps1
    v0.1 - First version. Simple Export of All GPOs in the domain. No filters, no OUs, no ACLs

---
+++ ADDS/Export-DomainGPOs.ps1
@@ -0,0 +1,11 @@
+
+Import-Module activedirectory
+Import-Module grouppolicy
+
+# Create Temp folder if it doesn't exist
+If (!(Test-Path -Path "c:\Temp" -ErrorAction SilentlyContinue )) {  New-Item "c:\Temp" -Type Directory -ErrorAction SilentlyContinue | Out-Null } 
+ 
+(Get-ADForest).domains | 
+  foreach { get-GPO -all -Domain $_ | 
+    Select-Object @{n='Domain Name';e={$_.DomainName}}, @{n='GPO Name';e={$_.DisplayName}}, @{n='GPO Guid';e={$_.Id}} , @{n='Gpo Status';e={$_.GpoStatus}} , @{n='Creation Time';e={$_.CreationTime}} , @{n='Modification Time';e={$_.ModificationTime}} } | 
+      Export-Csv c:\temp\AllGPOsList.csv
